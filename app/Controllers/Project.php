38f75b0239167126cb6f7d7dfab1bc1bb329c885
Diego Mariano <diogohenks@hotmail.com>
2024-11-15 12:04:38 -0300

    Update Project.php

--- app/Controllers/Project.php
+++ app/Controllers/Project.php
@@ -123,7 +123,7 @@ class Project extends BaseController
 		# START cocada PIPELINE *******************************************
 		system("python $raiz/app/ThirdParty/COCaDA/main.py -f $data_folder/$id/data.pdb -o $data_folder/$id");
 		system("python3 $raiz/app/ThirdParty/COCaDA/main.py -f $data_folder/$id/data.pdb -o $data_folder/$id");
-
+		system("pwd");
 		# renomeia o arquivo com a lista de contatos
 		system("mv $data_folder/$id/*.txt $data_folder/$id/contacts.csv");
 		dd("python3 $raiz/app/ThirdParty/COCaDA/main.py -f $data_folder/$id/data.pdb -o $data_folder/$id");
