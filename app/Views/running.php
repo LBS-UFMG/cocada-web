997267c607559d42b72764779181d3aa39dc12a7
Diego Mariano <diogohenks@hotmail.com>
2024-11-15 10:08:05 -0300

    correção no running

--- app/Views/running.php
+++ app/Views/running.php
@@ -5,10 +5,12 @@
 <div class="container py-5 text-secondary text-center">
     <h1>Running</h1>
     <p>Making a Cocada... wait...</p>
-    <img src="<?= base_url('img/cocadito.png'); ?>" width="500px">
-
     <p class="mt-4 alert alert-success"><b>Project created!</b><br><b>ID:</b> <?=$id?></p>
     <p>You will be redirected to the project page in <br><span id="contador" style="font-size: 50px;">10</span></h1>
+
+    <img src="<?= base_url('img/cocadito.png'); ?>" width="300px">
+
+    
 
 </div>
 
@@ -16,7 +18,7 @@
 <script>
     // Função para o redirecionamento
     function redirecionar() {
-        window.location.href = "/project/<?=$id?>";
+        window.location.href = "<?=base_url()?>/project/<?=$id?>";
     }
 
     // Função para o contador
