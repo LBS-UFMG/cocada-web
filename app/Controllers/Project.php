34a74804876ab5c980deea48566e65cd0b0cdb73
Diego Mariano <diogohenks@hotmail.com>
2024-11-15 12:19:44 -0300

    Update Project.php

--- app/Controllers/Project.php
+++ app/Controllers/Project.php
@@ -122,7 +122,7 @@ class Project extends BaseController
 
 		# START cocada PIPELINE *******************************************
 		// system("python $raiz/app/ThirdParty/COCaDA/main.py -f $data_folder/$id/data.pdb -o $data_folder/$id");
-		$saida = shell_exec("/usr/bin/python3 $raiz/app/ThirdParty/COCaDA/main.py -f $data_folder/$id/data.pdb -o $data_folder/$id");
+		$saida = shell_exec("/usr/bin/python3.6 $raiz/app/ThirdParty/COCaDA/main.py -f $data_folder/$id/data.pdb -o $data_folder/$id");
 		dd($saida);
 
 		
