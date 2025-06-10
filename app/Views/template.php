c0271ff71e6cdc32c4af162009277e46b25132d6
Diego Mariano <diogohenks@hotmail.com>
2025-06-10 18:26:28 -0300

    Update template.php

--- app/Views/template.php
+++ app/Views/template.php
@@ -18,7 +18,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
     <link rel="stylesheet" href="<?= base_url('css/estilo.css') ?>">
-    
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 </head>
 <body>
    
