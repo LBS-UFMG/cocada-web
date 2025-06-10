f6d3b12ca593d82e18c70795ac606a2c5bc38dc7
Diego Mariano <diogohenks@hotmail.com>
2025-06-10 17:55:17 -0300

    Update home.php

--- app/Views/home.php
+++ app/Views/home.php
@@ -163,21 +163,20 @@
   </div>
 
   <!-- advanced options -->
-  <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Advanced options
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        teste
+  <div class="accordion" id="adv_opt">
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Advanced options
+        </button>
+      </h2>
+      <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#adv_opt">
+        <div class="accordion-body">
+          teste
+        </div>
       </div>
     </div>
   </div>
-
-</div>
 
 
 </div>
