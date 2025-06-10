4099badda5240e0c40a49f21ea1a2ccc8cbfa5e3
Diego Mariano <diogohenks@hotmail.com>
2025-06-10 17:52:40 -0300

    Update home.php

--- app/Views/home.php
+++ app/Views/home.php
@@ -162,7 +162,24 @@
     </div>
   </div>
 
-  
+  <!-- advanced options -->
+  <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Advanced options
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        teste
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
 </div>
 
 <!-- ************************ EXAMPLES ************************ -->
