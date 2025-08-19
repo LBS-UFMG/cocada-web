b784d706cf4bf80c0763c44f4f824748c2bd25fe
Diego Mariano <diegomariano@ufmg.br>
2025-08-19 18:17:57 -0300

    Update Export.php

--- app/Controllers/Export.php
+++ app/Controllers/Export.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 class Export extends BaseController
 {
     public function pymol($id = 'null'){
+        # usada pelo controller project -> id tem 6 dígitos
         echo "<div class='bg-info w-100 p-2 m-0 small'><strong>Exporting $id to PyMOL...</strong><br>";
 
         # START cocada PIPELINE *******************************************
@@ -33,11 +34,13 @@ class Export extends BaseController
 	}
 
     public function pdb_to_pymol($id = 'null'){
+        # Esta função é usada pelo controller entry -> id tem 4 dígitos
         echo "<div class='bg-info w-100 p-2 m-0 small'><strong>Exporting contacts of $id to PyMOL...</strong><br>";
 
         # START cocada PIPELINE *******************************************
 		$data_folder = getcwd();
 		$raiz = str_replace("/public", "",$data_folder);
+        dd($data_folder);
 		$interpretador = "/home/liase/miniconda3/bin/python"; 
 
 		chmod("$raiz/public/data/pdb/$id[0]/$id", 0777); // quebra de segurança
