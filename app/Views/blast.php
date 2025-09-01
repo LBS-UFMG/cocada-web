ebafc3f5bd6174506f4b06cf14da47fc021ccd68
Diego Mariano <diegomariano@ufmg.br>
2025-09-01 09:34:45 -0300

    cocada logo para fundo branco

--- app/Views/blast.php
+++
@@ -1,9 +0,0 @@
-<?= $this->extend('template') ?>
-<?= $this->section('conteudo') ?>
-<!-- Conteúdo personalizado -->
-
-<div class="container text-secondary">
-    BLAST search will be implemented soon.
-</div>
-<!-- / FIM Conteúdo personalizado -->
-<?= $this->endSection() ?>
