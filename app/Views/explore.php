93f5633414c6a7f0a05a9de2350963e499db2dd1
Diego Mariano <diegomariano@ufmg.br>
2025-09-24 20:46:10 -0300

    Update explore.php

--- app/Views/explore.php
+++ app/Views/explore.php
@@ -30,7 +30,7 @@
                 <tbody>
                 </tbody>
             </table>
-            <p class="small text-muted text-center" id="loading_table">Loading data. Please wait.</p>
+            <p class="small text-muted text-center" id="loading_table">Loading data. Please wait.<br><div class="spinner-border spinner-border-sm" role="status"></div></p>
 
         </div>
     </div>
