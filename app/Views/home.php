7d004c7d49ae52d7ca62525760750af4ba62ed76
Diego Mariano <diegomariano@ufmg.br>
2025-09-24 18:13:57 -0300

    Update home.php

--- app/Views/home.php
+++ app/Views/home.php
@@ -138,12 +138,12 @@
 
   <h1><strong>Run COCαDA-web</strong></h1>
   <hr>
-  <p class="pb-4 text-muted">
+  <p class="pb-4 text-muted small">
     There are three ways to run COCaDA-web: (1) Upload a file in PDB or CIF format; (2) enter the PDB/UniProt code to download data from PDB/AlphaFoldDB API; (3) use the search bar to access COCaDA-db pre-calculated structure contacts. COCaDA-db only supports PDB structures with up to 10,000 amino acid residues. To access structures with more than 10,000 residues, please upload your file.
   </p>
   <div class="row">
     <div class="col-12 col-md-6 mt-2">
-      <label class="badge bg-primary">Submit your PDB file (limit 10MB)</label>
+      <label class="badge bg-dark">Submit your PDB file (limit 10MB)</label>
 
       <form action="<?php echo base_url('run'); ?>" method="post" enctype="multipart/form-data">
         <div class="input-group">
@@ -300,7 +300,7 @@
     </div>
   </div>
 
-  <button class="btn btn-success azul btn-lg  w-100 mt-4" type="submit" id="run">Run</button>
+  <button class="btn btn-success azul btn-lg  w-100 mt-5" type="submit" id="run">Run</button>
 
   </form>
 
