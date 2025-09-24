0e1806290c132ca4a6023b38cffe0e79a5721bc1
Diego Mariano <diegomariano@ufmg.br>
2025-09-24 18:38:48 -0300

    Update home.php

--- app/Views/home.php
+++ app/Views/home.php
@@ -139,7 +139,7 @@
   <h1><strong>Run COCαDA-web</strong></h1>
   <p class="lead">Calculate interatomic contacts for a protein file using the COCαDA-web interface</p>
   <p class="border rounded p-4 text-muted small">
-    <strong>There are three ways to run COCaDA-web</strong> (1) Upload a file in PDB or CIF format (below - left); (2) enter the PDB/UniProt code to download data from PDB/AlphaFoldDB API (below - right); (3) use the <a class="link-dark" href="<?=base_url('/#search_pdb')?>">search bar</a> (Examples section - right) to access COCaDA-db pre-calculated structure contacts. COCaDA-db only supports PDB structures with up to 10,000 amino acid residues. To access structures with more than 10,000 residues, please upload your file.
+    <strong>There are three ways to run COCaDA-web</strong>: (i) Upload a file in PDB or CIF format (below - left); (ii) enter the PDB/UniProt code to download data from PDB/AlphaFoldDB API (below - right); (iii) use the <a class="link-dark" href="<?=base_url('/#search_pdb')?>">search bar</a> (Examples section - right) to access COCaDA-db pre-calculated structure contacts. COCaDA-db only supports PDB structures with up to 10,000 amino acid residues. To access structures with more than 10,000 residues, please upload your file.
   </p>
   <div class="row">
     <div class="col-12 col-md-6 mt-2">
@@ -358,22 +358,24 @@
     <div class="col-12 col-md-6 mt-2">
       <p class="text-muted" id="search_pdb" >Or access an entry by typing the 4-characters PDB code <a class="link-dark" href="#" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-title="E.g.: 2LZM. Using this option, COCaDA-web will use preprocessed data from its internal database. Default parameters will be used for this processing. See the documentation for details. To change the parameters used, use the 'Run COCaDA-web' option."><i class="bi bi-question-circle-fill"></i></a>:</p>
       <div class="row g-2">
-        <div class="col-1">
-          <input type="text" id="code1" name="code1" class="form-control validacao" size="1" maxlength="1">
+        <div class="col-2">
+          <input type="text" id="code1" name="code1" class="form-control validacao p-3" size="1" maxlength="1">
         </div>
-        <div class="col-1">
-          <input type="text" id="code2" name="code2" class="form-control validacao" size="1" maxlength="1">
+        <div class="col-2">
+          <input type="text" id="code2" name="code2" class="form-control validacao p-3" size="1" maxlength="1">
         </div>
-        <div class="col-1">
-          <input type="text" id="code3" name="code3" class="form-control validacao" size="1" maxlength="1">
+        <div class="col-2">
+          <input type="text" id="code3" name="code3" class="form-control validacao p-3" size="1" maxlength="1">
         </div>
-        <div class="col-1">
-          <input type="text" id="code4" name="code4" class="form-control validacao" size="1" maxlength="1">
+        <div class="col-2">
+          <input type="text" id="code4" name="code4" class="form-control validacao p-3" size="1" maxlength="1">
         </div>
-        <div class="col-1">
-          <button type="button" id="go" class="btn btn-dark azul" name="go" value="Go to entry"><i class="bi bi-play-fill"></i></button>
+        <div class="col-2">
+          <button type="button" id="go" class="btn btn-dark azul  p-3" name="go" value="Go to entry"><i class="bi bi-play-fill"></i></button>
         </div>
       </div>
+      <span class="badge bg-secondary mb-1">PDB search bar</span>
+
     </div>
   </div>
 
