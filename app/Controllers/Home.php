035d5d60ebed6dc6cd1630744333b8ed4d38c10c
Diego Mariano <diogohenks@hotmail.com>
2026-09-10 16:51:56 +0200

    test

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -527,6 +527,7 @@ class Home extends BaseController
         $url = "https://files.rcsb.org/download/" . strtoupper($id) . "-assembly1.cif.gz";
 
         $gz = false;
+        dd('aqui');
         if (function_exists('curl_init')) {
             $ch = curl_init($url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
